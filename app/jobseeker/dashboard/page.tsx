34d45057c4884dfe9aab20b7af25e233a8a9e078
salwaaliakbar <salwaaliakbar22@gmail.com>
2026-02-13 22:30:00 +0500

    add digital employment page

---
+++ app/jobseeker/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import Dashboard from "@/components/jobseekerDashboard/Dashboard";
+import Header from "@/components/layout/Header";
+
+const JobseekerDashboardPage = () => {
+  return (
+    <>
+      <Header />
+      <Dashboard />
+    </>
+  );
+};
+
+export default JobseekerDashboardPage;
